99f5c3dee92924ab84be6cab752f070ec066889d
jlsandradebelpa@gmail.com <jorgeandradebelpa@gmail.com>
2021-06-14 21:53:07 -0300

    vários exercicios hoje

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,19 @@
+const a = 7
+let b = 3
+
+b += a
+console.log('b += a : ',b)
+
+b -= 4 // b = b - 4
+console.log('b -= 4 : ',b)
+
+b *= 2 // b = b * 2
+console.log('b *= 2 : ',b)
+
+b /= 2 // b = b / 2
+console.log('b /= 2 : ',b)
+
+b %= 2 // b = b % 2
+console.log('b %= 2 : ',b)
+
+console.log(a + b)

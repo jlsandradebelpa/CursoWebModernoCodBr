da0ed912b7456e5b05b4d7fe0200a95ab29d46d6
jlsandradebelpa@gmail.com <jorgeandradebelpa@gmail.com>
2021-06-18 22:12:41 -0300

    exercicios aritimeticos

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,8 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modeulo =a % 2
+console.log(soma,subtracao,multiplicacao,divisao,modeulo)

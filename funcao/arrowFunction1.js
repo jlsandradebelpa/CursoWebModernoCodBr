4dc252e4c795d530a48f3c60e23d6a08e569f7af
jlsandradebelpa@gmail.com <jorgeandradebelpa@gmail.com>
2021-06-22 20:33:05 -0300

    aula arrowFunction1,arrowFunction2

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,15 @@
+let dobro = function (a) {
+    return 2 * a
+}
+//console.log(dobro(1045))
+
+dobro = (a) => {
+    return 2 * a
+}
+//console.log(dobro(56))
+
+dobro = a => 2 * a
+console.log(dobro(Math.PI))
+
+ola = () => 'Olá'
+console.log(ola())

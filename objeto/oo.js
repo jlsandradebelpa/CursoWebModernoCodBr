bd367887677511a0dffbd9d7d21a5be9cf0664c9
jlsandradebelpa@gmail.com <jorgeandradebelpa@gmail.com>
2021-06-25 19:53:33 -0300

    aula oo

---
+++ objeto/oo.js
@@ -0,0 +1,27 @@
+// Código nao executável
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+Objeto = { 
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ..
+    }
+}
+
+Objeto.processamento()
+
+carro = {
+    placa,
+    marca,
+    ano,
+    fabricante,
+    renavam,
+    chassis
+}
+
+// Encapsulamento

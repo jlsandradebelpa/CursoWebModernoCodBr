f1a5cdef266206d3dd2c5f125d980e5adaa3b699
jlsandradebelpa@gmail.com <jorgeandradebelpa@gmail.com>
2021-06-28 13:54:56 -0300

    Aula : 118. Map #02

---
+++ array/map2.js
@@ -0,0 +1,20 @@
+/* 
+Arquivo: map2.js
+data : 27/06/2021 - 22:42
+Autor: Jorge Andrade
+Secao: 7 Javascript: Array 
+Aula : 118. Map #02
+*/
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }' 
+]    
+// Retonar um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)

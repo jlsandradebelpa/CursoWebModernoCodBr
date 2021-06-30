cca84c22f7898b0ace46b3d61df7da1fd9d8f98e
jlsandradebelpa@gmail.com <jorgeandradebelpa@gmail.com>
2021-06-29 23:05:03 -0300

    Aula : 127. Concat

---
+++ array/concat.js
@@ -0,0 +1,13 @@
+/* 
+Arquivo: Concat.js
+data : 28/06/2021 - 18:37
+Autor: Jorge Andrade
+Secao: 7 Javascript: Array 
+Aula : 127. Concat
+*/
+const filhas = ['Daleskah','Cibalena']
+const filhos = ['Uoxiton','Uesclei']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))

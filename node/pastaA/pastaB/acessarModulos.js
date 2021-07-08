2e0e36b595b852283dd5c0e7194fff213a7eb4cc
jlsandradebelpa <jorgeandradebelpa@gmail.com>
2021-07-08 19:41:29 -0300

    varias aulas

--- node/pastaA/pastaB/acessarModulos.js
+++ node/pastaA/pastaB/acessarModulos.js
@@ -1,11 +1,19 @@
+/* 
+Arquivo: acessarModulos.js
+data : 08/07/2021 - 19:06
+Autor: Jorge Andrade
+Secao: Seção 9: Node
+Aula : 134. Sistema de Módulos: Require
+*/
+const saudacao = require('saudacao')
 const ModuloA = require('../../moduloA')
 console.log(ModuloA.ola)
 
-const saudacao = require('saudacao')
-console.log(saudacao.log)
+//const saudacao = require('saudacao')
+//console.log(saudacao.ola)
 
 const c= require('./pastaC/index')
-console.log(c.ola)
+console.log(c.ola2)
 
 const http = require('http')
 http.createServer((req, res) => {

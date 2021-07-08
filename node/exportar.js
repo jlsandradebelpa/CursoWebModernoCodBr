9d39c299b547dadf20816298d7c91f1797774fef
jlsandradebelpa <jorgeandradebelpa@gmail.com>
2021-07-08 19:50:47 -0300

    Aula : 135. Sistema de Módulos: Exports

--- node/exportar.js
+++ node/exportar.js
@@ -1,2 +1,26 @@
+/* 
+Arquivo: exportar.js
+data : 08/07/2021 - 19:49
+Autor: Jorge Andrade
+Secao: Seção 9: Node
+Aula : 135. Sistema de Módulos: Exports
+*/
+console.log(module.exports)
 console.log(module.exports === this)
-console.log(module.exports === export)
+console.log(module.exports === exports)
+
+this.a = 1
+exports.b = 2
+module.exports.c = 3
+
+exports = null
+console.log(module.exports)
+
+
+exports = {
+    nome: 'Teste'
+}
+
+console.log(module.exports)
+
+module.exports = { publico: true}

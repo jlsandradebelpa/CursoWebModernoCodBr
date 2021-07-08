21e7e9bf29d84af05cb081b8936f5d8db3c2d908
jlsandradebelpa <jorgeandradebelpa@gmail.com>
2021-07-08 18:48:28 -0300

    varias aulas

--- node/pastaA/pastaB/acessarModulos.js
+++ node/pastaA/pastaB/acessarModulos.js
@@ -1,2 +1,14 @@
 const ModuloA = require('../../moduloA')
 console.log(ModuloA.ola)
+
+const saudacao = require('saudacao')
+console.log(saudacao.log)
+
+const c= require('./pastaC/index')
+console.log(c.ola)
+
+const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080)

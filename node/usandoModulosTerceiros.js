5efaa04f9a13ff768e7a8bc8ebec9b340778e3f0
jlsandradebelpa <jorgeandradebelpa@gmail.com>
2021-07-07 22:47:05 -0300

    varias aulas

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,9 @@
+/* 
+Arquivo: usandoModulosTerceiros.js
+data : 30/06/2021 - 21:35
+Autor: Jorge Andrade
+Secao: Seção 9: Node
+Aula : 133. Usando modulo de terceiros
+*/
+const _ = require('loadsh');
+setInterval(() => console.log(_.random(1, 10)),2000)

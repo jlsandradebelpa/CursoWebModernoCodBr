ab6e8ccd3f36b51798ad1a2581d0a6bc709adab8
jlsandradebelpa <jorgeandradebelpa@gmail.com>
2021-07-23 22:18:22 -0300

    Varias aulas

--- esnext/promises.js
+++ esnext/promises.js
@@ -1,7 +1,12 @@
-const { resolve } = require("path/posix");
-
 function falarDepoisDe(segundos, frase) {
-    return new Promise(resolve, reject => {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase)
+        }, segundos * 1000)
     })
 }
+
+falarDepoisDe(3, 'Que Legal!')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e))

8ca1bf7b80d84590cf46399ce2c4211bb0f3a484
jlsandradebelpa <jorgeandradebelpa@gmail.com>
2021-08-03 21:21:38 -0300

    // ESNEXT - Aula 187 - Promise #01

--- esnext/promise_1.js
+++ esnext/promise_1.js
@@ -1,16 +1,4 @@
 // ESNEXT - Aula 187 - Promise #01
-let p = new Promise(function(cumprirPromessa) {
-    cumprirPromessa(['Ana','Bia','Carlos','Daniel'])
-})
-
-p
-    .then(valor => valor[0])
-    .then(primeiro => primeiro[0])
-    .then(letra => letra.toLowerCase())
-    .then(letraMinuscula => console.log(letraMinuscula))
-
-
-/*
 const primeiroElemento = arrayOuString => arrayOuString[0]
 const letraMinuscula = letra => letra.toLowerCase()
 
@@ -20,5 +8,5 @@ new Promise(function(resolve) {
     .then(primeiroElemento)
     .then(primeiroElemento)
     .then(letraMinuscula)
+    // .then(v => console.log(v))
     .then(console.log)
-*/
